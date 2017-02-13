c9a75e54fa8ab5b79fc0a699330f34ad215045b5
Kristine Jensen <kristinejensen@Kristines-MacBook-Pro.local>
2017-02-12 21:27:34 -0600

    Initial Commit

---
+++ client.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('#submitButton').on('click', function(){
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var idNumber = $('#idNumber').val();
+    var jobTitle = $('#jobTitle').val();
+    var annualSalary = $('#annualSalary').val();
+    var employeeForm = firstName + ' ' + lastName + ' ' + idNumber + ' ' + jobTitle + ' ' + annualSalary;
+    $('#nameContainer').append('<p>' + employeeForm + " " +
+    '<button class="deleteButton">Delete</button></p>');
+  });
+  $('#submitButton').on('click', function() {
+    var monthlySalaryExpenditures = parseInt($('#annualSalary').val()) / 12;
+    $('#monthlySalaryCost').append(monthlySalaryExpenditures);
+  });
+  $('#submitButton').on('click', function(){
+    $('input').val('')
+  });
+  $('#nameContainer').on('click', '.deleteButton', function(){
+    $(this).parent().remove();
+  });
+});
